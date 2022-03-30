e4496892f0ed0d33e4c0abed08a9cfb564cf5ae9
admin <admin@CHETUIWK802>
2022-03-30 17:08:58 +0530

    Add project files.

---
+++ WebApplication3/Controllers/EmployeeController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private ApplicationDbContext DbContext;
+        public EmployeeController(ApplicationDbContext dbContext)
+        {
+            this.DbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<Employee> emps = DbContext.Employees.ToList();
+            return View(emps);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                DbContext.Employees.Add(emp);
+                DbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(emp);
+            }
+           
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var emp = DbContext.Employees.SingleOrDefault(e=> e.Id==id);
+
+            if (emp!=null)
+            {
+                DbContext.Employees.Remove(emp);
+                DbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        
+        public IActionResult Edit(int id)
+        {
+            var emp = DbContext.Employees.SingleOrDefault(e => e.Id == id);
+            return View (emp);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee emp)
+        {
+            DbContext.Employees.Update(emp);
+            DbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication3/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Display(Name="Employee Name")]
+        [Required(ErrorMessage ="Employee Name Can't Empty")]
+  
+        public string Name { get; set; }
+        [Display(Name = "Employee Gender")]
+        [Required(ErrorMessage = "Employee Gender Can't Empty")]
+
+        public string Gender { get; set; }
+        [Display(Name = "Employee Email")]
+        [Required(ErrorMessage = "Employee Email Address Can't Empty")]
+
+        public string Email { get; set; }
+        [Display(Name = "Employee Address")]
+        [Required(ErrorMessage = "Employee Address Can't Empty")]
+        public string Address { get; set; }
+    }
+}
